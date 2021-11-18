ccf139d52b991445857799b78e6a072d3b04f589
Prakhar-affinsys <89974091+Prakhar-affinsys@users.noreply.github.com>
2021-11-18 17:09:20 +0530

    updated

--- frontend/plotly.js
+++ frontend/plotly.js
@@ -60,8 +60,7 @@ async function myFunc() {
     }
     let trace1 = {
       x: x2,
-      y: yl,
-      type: 'bar'
+      y: yl
     };
     var layout = {font: {size: 18}};
     var config = {responsive: true};
