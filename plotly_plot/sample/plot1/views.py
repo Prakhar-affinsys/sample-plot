1ff2ff56fd47ad1935e36c222f9a41a51668b2dc
Prakhar-affinsys <89974091+Prakhar-affinsys@users.noreply.github.com>
2021-11-19 18:29:52 +0530

    e

--- plotly_plot/sample/plot1/views.py
+++ plotly_plot/sample/plot1/views.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 @api_view(['GET','POST'])
 def filter(request):
     df = request.data
+    print(df)
     val = df['age']
     qs = Merchant.objects.filter(age__gt = val).values()
     print(qs)
