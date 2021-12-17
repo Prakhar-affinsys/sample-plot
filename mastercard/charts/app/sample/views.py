163fb05f41331b19f06e2e45292949f7d461b090
Prakhar-affinsys <89974091+Prakhar-affinsys@users.noreply.github.com>
2021-12-17 16:41:02 +0530

    updated views

--- mastercard/charts/app/sample/views.py
+++ mastercard/charts/app/sample/views.py
@@ -2,14 +2,17 @@ from django.shortcuts import render
 from .models import Merchant
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from django.db.models import Sum
 # Create your views here.
 
 @api_view(['POST'])
 def filter(request):
     if(request.method == 'POST'):
-        qs = Merchant.objects.filter(**request.data).values()
-        print(qs)
-        return Response(qs)
+        #qs = Merchant.objects.filter(**request.data).values().count()
+        qs1 = Merchant.objects.filter(**request.data).aggregate(Sum('mer_code'))
+        print(qs1['mer_code__sum'])
+        return Response(qs1['mer_code__sum'])
     return Response(status=400)
 
     
